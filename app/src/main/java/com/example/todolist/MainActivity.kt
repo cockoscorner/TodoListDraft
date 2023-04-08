db5cd38fd3eea86a47e684680d579fc568573f22
cockos corner <cockocoxon@outlook.com>
2023-04-09 00:30:45 +0100

    misc fixes

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter: TodoAdaptor
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        todoAdapter = TodoAdaptor(mutableListOf())
+
+        binding.rvToDoItems.adapter = todoAdapter
+        binding.rvToDoItems.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddToDo.setOnClickListener {
+            val todoTitle = binding.etToDoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                binding.etToDoTitle.text.clear()
+            }
+        }
+
+        binding.btnDeleteToDo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
--- app/src/main/java/com/example/todolist/TodoAdaptor.kt
+++ app/src/main/java/com/example/todolist/TodoAdaptor.kt
@@ -1,8 +1,10 @@
 package com.example.todolist
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.ItemTodoBinding
 
@@ -10,24 +12,52 @@ class TodoAdaptor(
     private val todos: MutableList<Todo>
 ) : RecyclerView.Adapter<TodoAdaptor.TodoViewHolder>() {
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    private lateinit var binding: ItemTodoBinding
+    private lateinit var bindingTA: ItemTodoBinding
+    private lateinit var viewTA: View
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        return TodoViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_todo,
-                parent,
-                false
-            )
+
+        viewTA = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_todo,
+            parent,
+            false
         )
+        return TodoViewHolder(
+            viewTA
+        )
+    }
+
+    fun addTodo(todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDoneTodos() {
+        todos.removeAll { todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG.inv()
+        }
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
 
         val curTodo = todos[position]
+        bindingTA = ItemTodoBinding.bind(viewTA)
+        bindingTA.tvTodoTitle.text = curTodo.title
+        bindingTA.cbDone.isChecked = curTodo.isChecked
 
-        binding.tvTodoTitle.text = curTodo.title
-        binding.cbDone.isChecked = curTodo.isChecked
+        toggleStrikeThrough(bindingTA.tvTodoTitle, curTodo.isChecked)
+        bindingTA.cbDone.setOnCheckedChangeListener { _, isChecked ->
+            toggleStrikeThrough(bindingTA.tvTodoTitle, isChecked)
+        }
 
     }
 
