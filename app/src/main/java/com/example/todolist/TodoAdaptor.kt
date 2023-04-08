4a8c4afcc6fd279d53e5f9cabbbabd9a01b4787d
cockos corner <cockocoxon@outlook.com>
2023-04-08 20:52:38 +0100

    New binding

--- app/src/main/java/com/example/todolist/TodoAdaptor.kt
+++ app/src/main/java/com/example/todolist/TodoAdaptor.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTodoBinding
 
 class TodoAdaptor(
     private val todos: MutableList<Todo>
 ) : RecyclerView.Adapter<TodoAdaptor.TodoViewHolder>() {
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private lateinit var binding: ItemTodoBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         return TodoViewHolder(
@@ -21,11 +23,12 @@ class TodoAdaptor(
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-       val curTodo = todos[position]
 
-        holder.itemView.apply {
+        val curTodo = todos[position]
 
-        }
+        binding.tvTodoTitle.text = curTodo.title
+        binding.cbDone.isChecked = curTodo.isChecked
+
     }
 
     override fun getItemCount(): Int {
